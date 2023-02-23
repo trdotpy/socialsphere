a937d0f1e2300e35a9ec6b047621e116e1e47a28
trdotpy <102767855+trdotpy@users.noreply.github.com>
2023-02-22 19:18:14 -0500

    mobile nav

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
+            strokeWidth={2.0}
             stroke="currentColor"
             className="h-6 w-6 text-rose-500"
           >
